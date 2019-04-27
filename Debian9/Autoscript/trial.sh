f74c05690df788b648bcd37d60b23cbae3879546
Urabephc <50039753+Urabephc@users.noreply.github.com>
2019-04-27 14:58:01 +0800

    Update trial.sh

--- Debian9/Autoscript/trial.sh
+++ Debian9/Autoscript/trial.sh
@@ -14,12 +14,13 @@ echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
 echo -e "====Trial SSH Account====" | lolcat
 echo -e "Host: $IP" 
-echo -e "Port OpenSSH: 22,143"
-echo -e "Port Dropbear: 80,443"
-echo -e "Port Squid: 8080,3128"
-echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
+echo -e "Port OpenSSH: 22,444"
+echo -e "Port Dropbear: 3128,442"
+echo -e "Port Squid: 8080,, 80, 3128"
+echo -e "Config OpenVPN (TCP 1194): http://$IP/client.ovpn"
+echo -e "OpenVPN-Stunnel	: 587
 echo -e "Username: $Login"
 echo -e "Password: $Pass\n"
 echo -e "=========================" | lolcat
-echo -e "Mod by Clrkz"
+echo -e "Mod by urabe"
 echo -e ""
