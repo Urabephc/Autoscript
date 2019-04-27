b6c6b57aa80182eaa38afaa6b0cd97ce7b96b32a
Urabephc <50039753+Urabephc@users.noreply.github.com>
2019-04-27 15:16:31 +0800

    Add files via upload

---
+++ Debian9/Autoscript/about.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH,Dropbear,SSL,Squid,OpenVPN,OpenVPN-Stunnel" | lolcat 
+echo -e ""
+echo -e "For Debian 9 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "Mod by urabe"
+echo -e ""
+echo -e "https://www.phcorner.net/members/1232521/"
+echo -e ""
