67c179c585f424b34b579ba0c5a1aaf6999e403c
Urabephc <50039753+Urabephc@users.noreply.github.com>
2019-04-28 09:06:14 +0800

    Update Deb9vpsnocs.sh

--- Debian9/Autoscript/premiumscript/Deb9vpsnocs.sh
+++ Debian9/Autoscript/premiumscript/Deb9vpsnocs.sh
@@ -453,8 +453,8 @@ mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
 mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup 
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Urabephc/Autoscript/master/Debian9/Autoscript/premiumscript/nginx.conf" 
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Urabephc/Autoscript/master/Debian9/Autoscript/premiumscript/vps.conf" 
-sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini 
-sed -i 's/listen = \/var\/run\/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php7.0/fpm/pool.d/www.conf
+sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/fpm/php.ini 
+sed -i 's/listen = \/var\/run\/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.0/fpm/pool.d/www.conf
 
 useradd -m vps
 mkdir -p /home/vps/public_html
@@ -462,7 +462,7 @@ rm /home/vps/public_html/index.html
 rm /home/vps/public_html/index.php
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
-chmod -R g+rw /home/vps/public_html service php5-fpm restart
+chmod -R g+rw /home/vps/public_html service php7.0-fpm restart
 service php7.0-fpm restart
 service nginx restart
 
